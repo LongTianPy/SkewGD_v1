01baf39f542ad9d3275fce748faf0b98cd370658
LongTianPy <tianlongapp@gmail.com>
2016-03-25 14:11:17 -0400

    Correct ks Put non-duplicated pairs in to clusters

--- pycodeml.py
+++ pycodeml.py
@@ -8,13 +8,13 @@ from Bio.Phylo.PAML import yn00
 import sys
 
 def perform_codeml():
-    yn00 = yn00.Yn00()
-    yn00.alignment = "TF105351.Eut.3.phy"
-    yn00.tree = "TF105351.Eut.3.53876.tree"
-    yn00.out_file = "yn_result.out"
-    yn00.working_dir = "./"
-    yn00.read_ctl_file = "TF105351.Eut.3.53876.ctl"
-    run = yn00.run(command="/Users/longtian/Desktop/paml4.8/bin/yn00")
+    yn = yn00.Yn00()
+    yn.alignment = "TF105351.Eut.3.phy"
+    yn.tree = "TF105351.Eut.3.53876.tree"
+    yn.out_file = "yn_result.out"
+    yn.working_dir = "./"
+    yn.read_ctl_file = "TF105351.Eut.3.53876.ctl"
+    run = yn.run(command="/Users/longtian/Desktop/paml4.8/bin/yn00")
     return run
 
 if __name__=='__main__':
