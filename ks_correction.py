ceea3516f98e6f86d11ecffd3a87b2a9f090a52d
LongTianPy <tianlongapp@gmail.com>
2016-04-25 18:24:30 -0400

    Start merging

--- ks_correction.py
+++ ks_correction.py
@@ -154,11 +154,11 @@ def correct_ks(yn00):
     ks_df['kS_values'] = ks_per_cluster.values()
     return ks_df
 
-def draw_histo(ks_df):
+def draw_histo(ks_df, working_dir):
     size = len(ks_df)
     ax = seaborn.distplot(a=ks_df["kS_values"], kde=False,kde_kws={"color":"red"}, axlabel=False,bins=100)
     ax.set(xlabel="kS", ylabel= "Frequency", title="kS distribution",xlim=(0, 5))
-    plt.savefig("ks_distribution.pdf",format="pdf")
+    plt.savefig(working_dir+"ks_distribution.pdf",format="pdf")
     plt.clf()
 
 
